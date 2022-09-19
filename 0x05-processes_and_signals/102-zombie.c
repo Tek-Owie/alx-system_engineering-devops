1cb8b467e23000d1052d455679ec7002a540fbae
Emmanuel Owoicho Dickson <eodickson14@gmail.com>
2022-09-19 01:59:23 -0700

    Zombie

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -1,93 +1,47 @@
+#include <stdlib.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <sys/types.h>
-#include <sys/wait.h>
 #include <unistd.h>
 
 /**
- * infinite_while - Run an infinite while loop.
+ * infinite_while - Initializes an infinite while loop.
  *
- * Return: Always 0.
+ * Return: 0 if interrupted by a signal.
  */
 int infinite_while(void)
 {
-  while (1)
-    {
-      sleep(1);
-    }
-  return (0);
+while (1)
+{
+sleep(1);
+}
+return (0);
 }
 
 /**
- * main - Creates five zombie processes.
- *
- * Return: Always 0.
+ * create_process - Creates a new process and \
+ * prints the PID of the new process.
  */
-int main(void)
+void create_process(void)
 {
-  pid_t pid;
-  char count = 0;
+int rc = fork();
 
-  while (count < 5)
-    {
-      pid = fork();
-      if (pid > 0)
-	{
-	  printf("Zombie process created, PID: %d\n", pid);
-	  sleep(1);
-	  count++;
-	}
-      else
-	exit(0);
-    }
-
-  infinite_while();
-
-  return (EXIT_SUCCESS);
-}#include <stdio.h>
-#include <stdlib.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <unistd.h>
-
- /**
-  * infinite_while - Run an infinite while loop.
-  *
-  * Return: Always 0.
-  */
-int infinite_while(void)
+if (rc == 0)
 {
-  while (1)
-    {
-      sleep(1);
-    }
-  return (0);
+printf("Zombie process created, PID: %d\n", getpid());
+exit(0);
+}
 }
 
 /**
- * main - Creates five zombie processes.
+ * main - Creates 5 zombie processes.
  *
- * Return: Always 0.
+ * Return: 0 if successful
  */
 int main(void)
 {
-  pid_t pid;
-  char count = 0;
-
-  while (count < 5)
-    {
-      pid = fork();
-      if (pid > 0)
-	{
-	  printf("Zombie process created, PID: %d\n", pid);
-	  sleep(1);
-	  count++;
-	}
-      else
-	exit(0);
-    }
-
-  infinite_while();
-
-  return (EXIT_SUCCESS);
+create_process();
+create_process();
+create_process();
+create_process();
+create_process();
+return (infinite_while());
 }
